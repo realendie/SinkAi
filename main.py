bed1d94211f3277fd66b8ccd8d316eaf024d399a
realendie <enderproplays54@gmail.com>
2025-02-11 20:46:30 -0500

    replace Geologica font with Segoe UI, update GUI layout

--- main.py
+++ main.py
@@ -9,25 +9,29 @@ import darkdetect
 
 def callback(url):
     webbrowser.open_new(url)
+def set_text():
+    response = "Hello this is sample text!"
+    response_box.config(state=NORMAL)
+    response_box.insert(END, response)
+    response_box.config(state=DISABLED)
 
 
-font = Font(file="Geologica-VariableFont_CRSV,SHRP,slnt,wght.ttf", family=font)
-
+TESsT = "Test"
 
 root = tkinter.Tk()
 root.title("SinkAi")
-root.geometry("800x600")
+root.geometry("800x700")
 
 # Header // Author Link
 
-heading = tkinter.Label(root, text="SinkAi", font=(font, 10, "bold"), fg="black").pack()
+heading = tkinter.Label(root, text="SinkAi", font=('Segoe UI', 20, "bold"), fg="black").pack()
 
 author = tkinter.Label(
     root,
     text="By: @realendie",
-    font=(font, 10, "bold"),
+    font=('Segoe UI', 10, "bold"),
     fg="black",
-    cursor="hand10",
+    cursor='hand2',
 )
 author.bind("<Button-1>", lambda e: callback("https://github.com/realendie"))
 author.pack()
@@ -35,13 +39,16 @@ author.pack()
 # Input Box
 
 response_box = tkinter.Text(
-    root, width=100, font=(font, 10), fg="black", state=DISABLED
+    root, width=100, font=('Segoe UI', 10), fg="black", state=DISABLED,
 )
 response_box.pack()
 
-input_label = tkinter.Label(root, text="Input:", font=(font, 10), fg="black").pack()
+input_label = tkinter.Label(root, text="Input:", font=('Segoe UI', 10, "bold"), fg="black").pack()
 
-input_box = tkinter.Text(root, font=(font, 10), fg="black").pack()
+input_box = tkinter.Entry(root, font=('Segoe UI', 10), fg="black", width='100').pack()
+
+submit_button = tkinter.Button(root, text="Submit", font=('Segoe UI Semibold', 10), fg="black", command=set_text)
+submit_button.pack()
 
 sv_ttk.set_theme(darkdetect.theme())
 
